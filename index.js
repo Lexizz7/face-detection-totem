76758c21a36ec9746dd115a96b61d5488b72ac64
Lexizz7 <guetron@gmail.com>
2023-10-11 22:41:56 -0300

    Moldura, espelho da imagem, tradução e melhorias

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const video = document.getElementById("video");
+const videoWrapper = document.getElementById("video-wrapper");
+const loading = document.getElementById("loading");
 let canvas = null;
 let displaySize = null;
 
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
-    (stream) => (video.srcObject = stream),
+    (stream) => {
+      loading.style.display = "flex";
+      video.srcObject = stream;
+    },
     (err) => console.error(err),
   );
 }
@@ -24,15 +29,27 @@ function resize() {
 
 function init() {
   canvas = faceapi.createCanvasFromMedia(video);
-  document.body.append(canvas);
+  videoWrapper.append(canvas);
   resize();
+
   setInterval(async () => {
     const detections = await faceapi
       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
       .withFaceExpressions();
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    // Flip the detections horizontally so that they are mirrored
+    for (const detection of resizedDetections) {
+      detection.detection._box._x =
+        displaySize.width -
+        detection.detection._box._x -
+        detection.detection._box._width;
+      for (const landmark of detection.landmarks._positions) {
+        landmark._x = displaySize.width - landmark._x;
+      }
+    }
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    loading.style.display !== "none" && (loading.style.display = "none");
     faceapi.draw.drawDetections(canvas, resizedDetections);
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
